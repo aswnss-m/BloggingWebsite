4cca3f3f9207b932353e0689c77b52e6df23b178
aswnss <aswnss.m@gmail.com>
2022-09-18 13:26:56 +0530

    pages and routes creaeted

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../assets/CreatePost.css"
+
+function CreatePost() {
+  return (
+    <div>CreatePost</div>
+  )
+}
+
+export default CreatePost
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../assets/Login.css"
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
