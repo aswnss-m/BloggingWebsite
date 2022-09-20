45fa48ac0cd441453f643cf2140699b0f815dc73
aswnss <aswnss.m@gmail.com>
2022-09-20 11:16:19 +0530

    used useNavigate, working

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "../assets/Login.css"
 import { auth , provider } from "../firebase-config"
 import { signInWithPopup } from 'firebase/auth'
+import {useNavigate} from "react-router-dom"
 function Login({ setIsAuth }) {
+  const navigate = useNavigate()
   const signInWithGoogle = () => {
     signInWithPopup(auth,provider).then((result) => {
       localStorage.setItem("isAuth",true);
       setIsAuth(true)
-      window.location.pathname = "/"
+      navigate('/')
     })
   }
   return (
