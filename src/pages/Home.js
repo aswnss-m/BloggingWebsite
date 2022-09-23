ddfae268292dd115b76c5d5cb1d796611daf0811
aswnss <aswnss.m@gmail.com>
2022-09-23 10:50:39 +0530

    colors changed and

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import "../assets/Home.css"
-
+import PostsPage from '../components/PostsPage'
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <PostsPage />
+    </div>
   )
 }
 
